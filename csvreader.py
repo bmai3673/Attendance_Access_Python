728313779ff9acbcfe9b70d45f31d90ed6a5c7a5
bmai3673 <zomjobo@gmail.com>
2014-11-23 19:04:47 -0500

    Removed parts of the code, which includes the part that slices the CSV file and prints it, then made the program prompt the user for input. The program first asks what the user would like and then lists several options to choose from. The code is set to take the numbers that are assigned to each option and then print it when the user inputs a number(s) to display specific lists from the CSV file.

--- csvreader.py
+++ csvreader.py
@@ -2,9 +2,34 @@ import csv
 
 with open(r'C:\Python27\ENES100.CSV','rb') as csvfile:
     contents = csv.reader(csvfile)
+    var = raw_input("what do you need?\n" +
+                "1 Student Name\n" +
+                "2 ID Number\n" +
+                "3 Section\n" +
+                "4 Grade 1\n" +
+                "5 Grade 2\n" +
+                "6 Grade 3\n")
+    print (var)
+
     for students in contents:
-        want = students[0:2]
-        print ' '.join(want)
+        value = ""
 
+        if "1" in var:
+            value = value + students[0] + "; "
 
+        if "2" in var:
+            value = value + students[1] + "; "
 
+        if "3" in var:
+            value = value + students[2] + "; "
+
+        if "4" in var:
+            value = value + students[3] + "; "
+
+        if "5" in var:
+            value = value + students[4] + "; "
+
+        if "6" in var:
+            value = value + students[5] + "; "
+            
+        print value
